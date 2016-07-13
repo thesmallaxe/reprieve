5cafea0979903fe344e66d5981bbcfb2385c3f3e
Ahamed Aaqil <aaqil@thesmallaxe.com>
2016-07-13 19:56:20 +0530

    Script bug fix test

--- javascript.js
+++ javascript.js
@@ -15,35 +15,65 @@ var currentscrollpos;
 
 $('.title1').on('click', function(event) {
     closePopUp();
-    $(".mobile-tooltip-1").addClass('poped-up');
+    if ($(".mobile-tooltip-1").hasClass('poped-up')) {
+        $(".poped-up").removeClass('poped-up');
+    } else {
+        $(".poped-up").addClass('poped-up');
+    }
+    if ($(".mobile-tooltip-1").hasClass('poped-up')) {
+        $(".poped-up").removeClass('poped-up');
+    } else {
+        $(".poped-up").addClass('poped-up');
+    }
+    // $(".mobile-tooltip-1").addClass('poped-up');
     $('.mobile-tooltip-1').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title2').on('click', function(event) {
     closePopUp();
-    $(".mobile-tooltip-2").addClass('poped-up');
+    if ($(".mobile-tooltip-2").hasClass('poped-up')) {
+        $(".poped-up").removeClass('poped-up');
+    } else {
+        $(".poped-up").addClass('poped-up');
+    }
+    // $(".mobile-tooltip-2").addClass('poped-up');
     $('.mobile-tooltip-2').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title3').on('click', function(event) {
     closePopUp();
-    $(".mobile-tooltip-3").addClass('poped-up');
+    if ($(".mobile-tooltip-3").hasClass('poped-up')) {
+        $(".poped-up").removeClass('poped-up');
+    } else {
+        $(".poped-up").addClass('poped-up');
+    }
+    // $(".mobile-tooltip-3").addClass('poped-up');
     $('.mobile-tooltip-3').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title4').on('click', function(event) {
     closePopUp();
-    $(".mobile-tooltip-4").addClass('poped-up');
+    if ($(".mobile-tooltip-4").hasClass('poped-up')) {
+        $(".poped-up").removeClass('poped-up');
+    } else {
+        $(".poped-up").addClass('poped-up');
+    }
+    // $(".mobile-tooltip-4").addClass('poped-up');
     $('.mobile-tooltip-4').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title5').on('click', function(event) {
     closePopUp();
-    $(".mobile-tooltip-5").addClass('poped-up');
+    if ($(".mobile-tooltip-5").hasClass('poped-up')) {
+        $(".poped-up").removeClass('poped-up');
+    } else {
+        $(".poped-up").addClass('poped-up');
+    }
+    // $(".mobile-tooltip-5").addClass('poped-up');
     $('.mobile-tooltip-5').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
