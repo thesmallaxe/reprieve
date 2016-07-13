e6216992a3d1f6a6f2fafc37bd400a4fded42ee9
Georgia O'Brien <georgia@thesmallaxe.com>
2016-07-13 11:26:42 +0100

    Quick mobile versions

--- javascript.js
+++ javascript.js
@@ -3,9 +3,38 @@ $('.title1').on('click', function(event) {
 });
 
 $('.title2').on('click', function(event) {
-	$(".mobile-tooltip-2").css('display', 'block');
+	$('.mobile-tooltip-2').toggle('show');
 });
 
+$('.title3').on('click', function(event) {
+	$('.mobile-tooltip-3').toggle('show');
+});
+
+$('.title4').on('click', function(event) {
+	$('.mobile-tooltip-4').toggle('show');
+});
+
+$('.title5').on('click', function(event) {
+	$('.mobile-tooltip-5').toggle('show');
+});
+
+
 $('.close1').on('click', function(event) {
+	$(".mobile-tooltip-1").css('display', 'none');
+});
+
+$('.close2').on('click', function(event) {
 	$(".mobile-tooltip-2").css('display', 'none');
+});
+
+$('.close3').on('click', function(event) {
+	$(".mobile-tooltip-3").css('display', 'none');
+});
+
+$('.close4').on('click', function(event) {
+	$(".mobile-tooltip-4").css('display', 'none');
+});
+
+$('.close5').on('click', function(event) {
+	$(".mobile-tooltip-5").css('display', 'none');
 });
