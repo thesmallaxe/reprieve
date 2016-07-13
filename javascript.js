0b72f4e1adb9e2732b4e324cecbe8bb02408ce2c
Ahamed Aaqil <aaqil@thesmallaxe.com>
2016-07-13 19:15:12 +0530

    Script Update

--- javascript.js
+++ javascript.js
@@ -1,47 +1,47 @@
-var currentscrollpos;
-
-$('.title1').on('click', function(event) {
-    // $('.mobile-tooltip-1').toggle('show');
-    // currentscrollpos = $(window).scrollTop();
-
+function closePopUp() {
     if ($(".poped-up").length > 0) {
-        console.log('test');
-        // Do stuff with $(".Mandatory")
         $(this).each(function() {
             console.log('test2');
-            // "this" points to current item in looping through all elements with
-            // class="Mandatory"
             if ($(".poped-up").css('display') == 'block') {
-                //your code
                 console.log('test3');
                 $(".poped-up").css('display', 'none');
             }
-
         });
     }
+}
+
+var currentscrollpos;
+
+$('.title1').on('click', function(event) {
+    closePopUp();
     $(".mobile-tooltip-1").addClass('poped-up');
     $('.mobile-tooltip-1').toggle('show');
+    currentscrollpos = $(window).scrollTop();
 });
 
 $('.title2').on('click', function(event) {
+    closePopUp();
     $('.mobile-tooltip-2').toggle('show');
     $(".mobile-tooltip-2").addClass('poped-up');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title3').on('click', function(event) {
+    closePopUp();
     $('.mobile-tooltip-3').toggle('show');
     $(".mobile-tooltip-3").addClass('poped-up');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title4').on('click', function(event) {
+    closePopUp();
     $('.mobile-tooltip-4').toggle('show');
     $(".mobile-tooltip-4").addClass('poped-up');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title5').on('click', function(event) {
+    closePopUp();
     $('.mobile-tooltip-5').toggle('show');
     $(".mobile-tooltip-5").addClass('poped-up');
     currentscrollpos = $(window).scrollTop();
