3aa1305675dfbf16dac62fb9b52ed43f0352ab89
Georgia O'Brien <georgia@thesmallaxe.com>
2016-07-13 16:31:48 +0100

    Add tooltip.js

---
+++ tooltip.js
@@ -0,0 +1,82 @@
+var currentscrollpos;
+
+$('.title1').on('click', function(event) {
+	if($(window).width() >= 768) {
+	$(".mobile-tooltip-2").css('display', 'none');
+	$(".mobile-tooltip-3").css('display', 'none');
+	$(".mobile-tooltip-4").css('display', 'none');
+	$(".mobile-tooltip-5").css('display', 'none');
+}
+	$('.mobile-tooltip-1').toggle('show');
+	currentscrollpos = $(window).scrollTop();
+});
+
+$('.title2').on('click', function(event) {
+	if($(window).width() >= 768) {
+	$(".mobile-tooltip-1").css('display', 'none');
+	$(".mobile-tooltip-3").css('display', 'none');
+	$(".mobile-tooltip-4").css('display', 'none');
+	$(".mobile-tooltip-5").css('display', 'none');
+	}
+	$('.mobile-tooltip-2').toggle('show');
+	currentscrollpos = $(window).scrollTop();
+});
+
+$('.title3').on('click', function(event) {
+	if($(window).width() >= 768) {
+	$(".mobile-tooltip-1").css('display', 'none');
+	$(".mobile-tooltip-2").css('display', 'none');
+	$(".mobile-tooltip-4").css('display', 'none');
+	$(".mobile-tooltip-5").css('display', 'none');
+}
+	$('.mobile-tooltip-3').toggle('show');
+	currentscrollpos = $(window).scrollTop();
+});
+
+$('.title4').on('click', function(event) {
+	if($(window).width() >= 768) {
+	$(".mobile-tooltip-1").css('display', 'none');
+	$(".mobile-tooltip-2").css('display', 'none');
+	$(".mobile-tooltip-3").css('display', 'none');
+	$(".mobile-tooltip-5").css('display', 'none');
+}
+	$('.mobile-tooltip-4').toggle('show');
+	currentscrollpos = $(window).scrollTop();
+});
+
+$('.title5').on('click', function(event) {
+	if($(window).width() >= 768) {
+	$(".mobile-tooltip-1").css('display', 'none');
+	$(".mobile-tooltip-2").css('display', 'none');
+	$(".mobile-tooltip-3").css('display', 'none');
+	$(".mobile-tooltip-4").css('display', 'none');
+}
+	$('.mobile-tooltip-5').toggle('show');
+	currentscrollpos = $(window).scrollTop();
+});
+
+
+$('.close1').on('click', function(event) {
+	$(".mobile-tooltip-1").css('display', 'none');
+	$("html, body").animate({ scrollTop: currentscrollpos }, 0);
+});
+
+$('.close2').on('click', function(event) {
+	$(".mobile-tooltip-2").css('display', 'none');
+	$("html, body").animate({ scrollTop: currentscrollpos }, 0);
+});
+
+$('.close3').on('click', function(event) {
+	$(".mobile-tooltip-3").css('display', 'none');
+	$("html, body").animate({ scrollTop: currentscrollpos }, 0);
+});
+
+$('.close4').on('click', function(event) {
+	$(".mobile-tooltip-4").css('display', 'none');
+	$("html, body").animate({ scrollTop: currentscrollpos }, 0);
+});
+
+$('.close5').on('click', function(event) {
+	$(".mobile-tooltip-5").css('display', 'none');
+	$("html, body").animate({ scrollTop: currentscrollpos }, 0);
+});
