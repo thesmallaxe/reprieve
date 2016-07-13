6fcc127c75e48bcd66de0d1db5cd0959170e95a2
Ahamed Aaqil <aaqil@thesmallaxe.com>
2016-07-13 20:28:36 +0530

    test

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 function closePopUp() {
-    if ($(".mobile-tooltip").length > 0) {
+    if ($(".poped-up").length > 0) {
         $(this).each(function() {
-            if ($(".mobile-tooltip").css('display') == 'block') {
-                $(".mobile-tooltip").css('display', 'none');
-                // $(".poped-up").removeClass('poped-up');
+            if ($(".poped-up").css('display') == 'block') {
+                $(".poped-up").css('display', 'none');
+                $(".poped-up").removeClass('poped-up');
             }
         });
     }
@@ -13,41 +13,35 @@ var currentscrollpos;
 
 $('.title1').on('click', function(event) {
     closePopUp();
-    // $(".mobile-tooltip-1").addClass('poped-up');
-    // $(".mobile-tooltip-1").toggleClass("poped-up");
+    $(".mobile-tooltip-1").addClass('poped-up');
     $('.mobile-tooltip-1').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title2').on('click', function(event) {
     closePopUp();
-    // $(".mobile-tooltip-2").addClass('poped-up');
-    // $(".mobile-tooltip-2").toggleClass("poped-up");
+    $(".mobile-tooltip-2").addClass('poped-up');
     $('.mobile-tooltip-2').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title3').on('click', function(event) {
     closePopUp();
-    // $(".mobile-tooltip-3").addClass('poped-up');
-    // $(".mobile-tooltip-3").toggleClass("poped-up");
+    $(".mobile-tooltip-3").addClass('poped-up');
     $('.mobile-tooltip-3').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title4').on('click', function(event) {
     closePopUp();
-    // $(".mobile-tooltip-4").addClass('poped-up');
-
-    // $(".mobile-tooltip-4").toggleClass("poped-up");
+    $(".mobile-tooltip-4").addClass('poped-up');
     $('.mobile-tooltip-4').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title5').on('click', function(event) {
     closePopUp();
-    // $(".mobile-tooltip-5").addClass('poped-up');
-    // $(".mobile-tooltip-5").toggleClass("poped-up");
+    $(".mobile-tooltip-5").addClass('poped-up');
     $('.mobile-tooltip-5').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
