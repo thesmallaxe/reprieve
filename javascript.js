5e4a43db4839fdb9af7e3c4b974f2b03d4f67380
Ahamed Aaqil <aaqil@thesmallaxe.com>
2016-07-13 19:43:10 +0530

    script checking bugs

--- javascript.js
+++ javascript.js
@@ -14,41 +14,37 @@ function closePopUp() {
 var currentscrollpos;
 
 $('.title1').on('click', function(event) {
-    closePopUp();
+    // closePopUp();
     $(".mobile-tooltip-1").addClass('poped-up');
     $('.mobile-tooltip-1').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title2').on('click', function(event) {
-    closePopUp();
-
+    // closePopUp();
     $(".mobile-tooltip-2").addClass('poped-up');
     $('.mobile-tooltip-2').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title3').on('click', function(event) {
-    closePopUp();
-
+    // closePopUp();
     $(".mobile-tooltip-3").addClass('poped-up');
     $('.mobile-tooltip-3').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title4').on('click', function(event) {
-    closePopUp();
-
+    // closePopUp();
     $(".mobile-tooltip-4").addClass('poped-up');
     $('.mobile-tooltip-4').toggle('show');
     currentscrollpos = $(window).scrollTop();
 });
 
 $('.title5').on('click', function(event) {
-    closePopUp();
+    // closePopUp();
     $(".mobile-tooltip-5").addClass('poped-up');
     $('.mobile-tooltip-5').toggle('show');
-
     currentscrollpos = $(window).scrollTop();
 });
 
