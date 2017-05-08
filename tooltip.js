f507c909b39b25234227abb36c28b93b52ef8c3c
Georgia O'Brien <georgiacobrien@gmail.com>
2017-05-08 11:02:43 +0100

    Add timer

--- tooltip.js
+++ tooltip.js
@@ -210,6 +210,33 @@ $('.give-monthly .other input').change(function(){
   $(".give-monthly .other .donate-link").attr("href","https://secure.edirectdebit.com/Reprieve/Donate2/Desktop-Form-Page/?default_amt=" + var2)
 });
 
+var timeInSecs;
+var ticker;
+
+function startTimer(secs) {
+timeInSecs = parseInt(secs);
+ticker = setInterval("tick()", 1000); 
+}
+
+function tick( ) {
+var secs = timeInSecs;
+if (secs > 0) {
+timeInSecs--; 
+}
+else {
+clearInterval(ticker);
+startTimer(4*60); // 4 minutes in seconds
+}
+
+var hours= Math.floor(secs/3600);
+secs %= 3600;
+var mins = Math.floor(secs/60);
+secs %= 60;
+var pretty = ( (hours < 10 ) ? "0" : "" ) + hours + ":" + ( (mins < 10) ? "0" : "" ) + mins + ":" + ( (secs < 10) ? "0" : "" ) + secs;
+document.getElementById("countdown").innerHTML = pretty;
+}
+
+startTimer(240*60);
 
 
 
