42ab48e52c7fe8805b7d7b2ef91e8fbb5b2b943f
Georgia O'Brien <georgiacobrien@gmail.com>
2017-05-08 22:54:12 +0100

    Update tooltip.js

--- tooltip.js
+++ tooltip.js
@@ -210,6 +210,8 @@ $('.give-monthly .other input').change(function(){
   $(".give-monthly .other .donate-link").attr("href","https://secure.edirectdebit.com/Reprieve/Donate2/Desktop-Form-Page/?default_amt=" + var2)
 });
 
+$("#countdown-container").prependTo(".navcol");
+
 var timeInSecs;
 var ticker;
 
