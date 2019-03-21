829e30fa15329207bd93e2183982c0c53771f6f4
Georgia O'Brien <georgiacobrien@gmail.com>
2019-03-21 17:08:59 +0000

    Update tooltip.js

--- tooltip.js
+++ tooltip.js
@@ -273,7 +273,7 @@ if (getCookie('domainGUID')) {
 }
 
 // Homepage edits
-$('.surnav .topnav_block.secnav.last').before('<li class="topnav_block secnav"><a href="https://act.reprieve.org.uk/page/contribute/dec18-sl?source=rukhome" target="_blank" class="topnav_element secnav">Donate</a></li>');
+$('.surnav .topnav_block.secnav.last').before('<li class="topnav_block secnav"><a href="https://act.reprieve.org.uk/page/contribute/donatenow-sl?source=rukhome" target="_blank" class="topnav_element secnav">Donate</a></li>');
 
 $(".primary h1, .primary h2, .section_subtitle").wrapInner("<span></span>");
 
